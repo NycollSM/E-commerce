23c4610077ee843d417dec170f14aed620549904
nycollSM <nycollsotomorales21@gmail.com>
2019-11-08 15:03:19 -0600

    added sass as dependecy and variables

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './sass/styles.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+      <div>
+        <h1>font size test</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
